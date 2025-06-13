fa1924cb6ccc0a5d9025f66e225fe24e3e9adcbe
harshsaxena40601 <146015392+harshsaxena40601@users.noreply.github.com>
2025-06-13 17:11:01 +0530

    5.

--- emailapi/emailapi/urls.py
+++ emailapi/emailapi/urls.py
@@ -9,12 +9,16 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 # from api.views import collect_user_info  # Your custom view
+def homepage(request):
+    return HttpResponse("✅ Welcome to Editopia Studio!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin panel
     # path('api/submit/', collect_user_info),  # Custom API view
     path('api/', include('api.urls')),  # Additional API routes from api/urls.py
+     path('', homepage),  # ✅ Root homepage route
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
